a3154fb34caa772fdeb52b436acd52fca7e26810
Max Brandstetter <middlestetter999@gmail.com>
2016-11-18 16:20:32 -0800

    Finished REST service

---
+++ MyLabService/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Lab_2.Models;
+using Lab_2.Data;
+
+namespace MyLabService.Controllers
+{
+    public class UserController : ApiController
+    {
+        private IUserRepository _userRepository;
+
+        public UserController()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public List<User> GetAllUsers()
+        {
+            List<User> users = _userRepository.GetUsers();
+
+            return users;
+        }
+
+        public IHttpActionResult GetUser(int id)
+        {
+            var user = _userRepository.GetUser(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
---
+++ MyLabService/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyLabService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("MyLabService")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e31dcc79-93a3-4c77-8ab0-07b5489e573e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
